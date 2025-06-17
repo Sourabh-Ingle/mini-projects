79e0deebfac211a4c7de772978112c241c762fce
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-06-18 02:34:58 +0530

    coupon code mini project

---
+++ proj-coupan-card/script.js
@@ -0,0 +1,60 @@
+let openCoupon = document.querySelector('.open-coupon');
+let applyCoupon = document.querySelector('.apply-coupon');
+let couponInput = document.getElementById('coupon-input');
+let couponCard = document.querySelector('.coupon-card');
+let couponClose = document.querySelector('.coupon-close');
+let copyBtn = document.querySelector('#copy-btn');
+let copyCode = document.querySelector('#copy-code').innerText;
+let priceDesc = document.querySelector('.price-desc');
+let price = document.querySelector('.price');
+let disCount = document.querySelector('.discount').innerText;
+let alertMessage = document.querySelector('.alert-message');
+
+openCoupon.addEventListener('click', handleOpenCoupon);
+copyBtn.addEventListener('click', handleCopyCoupon);
+applyCoupon.addEventListener('click', handleDiscontedPrice);
+
+
+function handleOpenCoupon() {
+    couponCard.classList.add('coupan-active');
+}
+
+function handleCloseCoupon() {
+    couponCard.classList.remove('coupan-active');
+}
+
+function handleCopyCoupon() {
+    navigator.clipboard.writeText(copyCode);
+    copyBtn.innerText = "Copied";
+    setTimeout(() => { copyBtn.innerText = "Copy Code"; }, 2000);
+}
+
+function handleDiscontedPrice() {
+    //   let descPrice= priceDesc.innerText.split(" ");
+    //   let price = descPrice.find(item=>!isNaN(item)) ; //if there is whole string but in many cases we got direct price
+    let prevPrice = price.innerText;
+    let discountedPrice = prevPrice * (100 - disCount) / 100;
+
+    if (couponInput.value === copyCode) {
+        applyCoupon.setAttribute('disabled', '');
+        price.innerHTML = `<span class="cancel-price"> Rs ${prevPrice}</span> <span class="discount-price"> ${discountedPrice}</span>`;
+        handleAlertMessage('success', "coupon code applied. please check price");
+    } else if (couponInput.value === '') {
+        handleAlertMessage('success', "No Coupon code applied");
+    } else {
+        handleAlertMessage('danger', "Your coupon code is not valid");
+    }
+
+}
+
+function handleAlertMessage(action, message) {
+    alertMessage.classList.add(`action-${action}`);
+    alertMessage.innerText = message;
+
+    setTimeout(() => {
+        alertMessage.classList.remove(`action-${action}`);
+        alertMessage.innerText = '';
+    }, 2000)
+}
+
+
