43c568fcd30d19a216611bb3f360ac5bd0772895
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-06-20 23:01:22 +0530

    add js with handleAlert handleCountElem and handleShowOutput

--- proj-countElem/app.js
+++ proj-countElem/app.js
@@ -6,3 +6,54 @@ let alert = document.querySelector('.alert');
 let newStart = document.querySelector('.new-start');
 
 let arr = [];
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    let numValue = inputValue.value;
+    inputValue.value = '';
+    if (!isNaN(numValue) && numValue) {
+        handleCountElem(numValue);
+        handleShowOutput(showInput, numValue);
+    } else if (numValue === "") {
+        handleAlert("danger", ` Plz enter valid number value.`)
+    }
+    else {
+        handleAlert("danger", ` Plz enter valid number. (${numValue}) is not number!!!`)
+    }
+
+})
+
+function handleCountElem(num) {
+
+    let countObj = {};
+    arr.push(num);
+
+    arr.forEach((item) => {
+        countObj[item] = (countObj[item] || 0) + 1;
+    })
+    let stringObj = JSON.stringify(countObj);
+    handleShowOutput(showResult, stringObj ,0)
+
+}
+
+function handleShowOutput(eleVal, num,val) {
+    if (val===0) {
+        eleVal.innerText = ``;
+    }
+
+    eleVal.innerText += ` ${num} ,`;
+}
+
+function handleAlert(action, text) {
+    alert.innerText = text;
+    alert.classList.add(`action-${action}`);
+    setTimeout(() => {
+        alert.innerText = "";
+        alert.classList.remove(`action-${action}`);
+    }, 1000)
+}
+
+newStart.addEventListener('click',()=>{
+     arr = [];
+     showInput.innerText=``;
+     showResult.innerText=``;
+})
