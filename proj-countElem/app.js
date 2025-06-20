dc94938f4d2f51595caa3368a59c20fd2cfefcf4
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-06-20 22:59:51 +0530

    add css and html

---
+++ proj-countElem/app.js
@@ -0,0 +1,8 @@
+let inputValue = document.getElementById('num');
+let showInput = document.querySelector('.showArr');
+let showResult = document.querySelector('.count-num');
+let submit = document.getElementById('submit');
+let alert = document.querySelector('.alert');
+let newStart = document.querySelector('.new-start');
+
+let arr = [];
