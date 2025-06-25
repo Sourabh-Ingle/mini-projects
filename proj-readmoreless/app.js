167db8a02df97ae94c8248b4c3a525bb12cc69ef
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-06-25 23:17:51 +0530

    add and changes in logic and functionality

--- proj-readmoreless/app.js
+++ proj-readmoreless/app.js
@@ -1,2 +1,31 @@
 let btn = document.querySelectorAll('.btn');
 
+btn.forEach(element => {
+    element.addEventListener('click', (e) => handleshow(e));
+});
+
+document.querySelectorAll('.desc').forEach(p => {
+    const full = p.textContent;
+    p.dataset.fulltext = full;
+    p.textContent = truncateText(full, 100);
+});
+
+function handleshow(e) {
+    const card = e.target.closest('.card'); 
+    const desc = card.querySelector('.desc');
+
+    const fullText = desc.dataset.fulltext || desc.textContent;
+    if (e.target.textContent === 'Read more') {
+        desc.textContent = fullText;
+        e.target.textContent = 'Read less';
+        desc.dataset.fulltext = fullText; 
+    } else {
+        desc.textContent = truncateText(fullText, 100);
+        e.target.textContent = 'Read more';
+    }
+
+}
+
+function truncateText(text, maxLength) {
+    return text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
+}
