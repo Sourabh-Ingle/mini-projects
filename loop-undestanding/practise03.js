a25b3c7408372188920c3424ee36180322beacf2
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-07-19 00:04:20 +0530

    FIND SECOND LARGEST NUMBER IN ARR,FIND FIRST,SECOND,THIRD LARGEST IN OBJ

--- loop-undestanding/practise03.js
+++ loop-undestanding/practise03.js
@@ -21,3 +21,50 @@ let largestNumberOfArr = largestNumber(arr3)
 console.log("Largest Number in arr is : ", largestNumberOfArr);
 
 
+// QUES--->a)FIND SECOND LARGEST NUMBER IN ARR
+//         b)FIND FIRST,SECOND,THIRD LARGEST IN OBJ
+
+const arr4 = [1, 2, 3, 9, 6, 6, 10, 10, 9, 10];
+const threeLargestNumObj = { "first": undefined, "second": undefined, "third": undefined };
+
+function secondLargest(arr) {
+    let largestNum = arr[0];
+    let secondLargestNumber;
+    let thirdLargestNumber;
+    for (let i = 1; i < arr.length - 1; i++) {
+        if (arr[i] === largestNum) {
+            largestNum = arr[i];
+            threeLargestNumObj.first = arr[i];
+        }
+        else if (arr[i] > largestNum) {
+            // third largest
+            thirdLargestNumber = secondLargestNumber;
+            threeLargestNumObj.third = thirdLargestNumber;
+            // second largest
+            secondLargestNumber = largestNum;
+            threeLargestNumObj.second = secondLargestNumber;
+
+            // first latgest
+            largestNum = arr[i];
+            threeLargestNumObj.first = arr[i];
+        }
+        else {
+            if (arr[i] > secondLargestNumber) {
+                thirdLargestNumber = secondLargestNumber;
+                threeLargestNumObj.third = secondLargestNumber;
+                secondLargestNumber = arr[i];
+                threeLargestNumObj.second = arr[i];
+            }
+            if (arr[i] === secondLargestNumber) {
+                secondLargestNumber = arr[i];
+                threeLargestNumObj.second = arr[i];
+            }
+        }
+    }
+
+    return secondLargestNumber;
+}
+
+let secondLargestNum = secondLargest(arr4);
+console.log("The second largest number is : ", secondLargestNum);
+console.log("The largest 3 number is : ", threeLargestNumObj);
