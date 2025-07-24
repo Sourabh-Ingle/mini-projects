67350b7073bb422717730dc467d5280bd6a6c654
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-07-25 02:04:50 +0530

    GIVEN INTEGER IS POWER OF TWO

---
+++ loop-undestanding/practise13.js
@@ -0,0 +1,24 @@
+//  IS GIVEN INTEGER IS POWER OF 2 FIND
+// Given an integer n, return true if it is a power of two.Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2**x.
+// Example 1:
+
+// Input: n = 1
+// Output: true
+// Explanation: 20 = 1
+
+// Example 2:
+
+// Input: n = 16
+// Output: true
+// Explanation: 24 = 16
+
+function isPowerOfTwo(n) {
+    if (n === 1) return true
+    else if(n<0||n%2!==0){
+       return false
+    }
+    return isPowerOfTwo(n/2)
+}
+console.log(isPowerOfTwo(8))
