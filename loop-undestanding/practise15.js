1700de8f96198fe6ad89d4e24341b1d4030a0aa5
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-07-28 20:12:39 +0530

    BINARY SEARCH

---
+++ loop-undestanding/practise15.js
@@ -0,0 +1,22 @@
+// BINARY SEARCH
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+function binarySearch(a, num) {
+    
+    let left = 0;
+    let right = a.length - 1;
+    while (right >= left) {
+        let mid = Math.floor((right + left) / 2);
+        if (a[mid] === num) {
+            return mid;
+        } else if (num < a[mid]) {
+            right = mid - 1;
+        } else {
+            left=mid+1
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch(arr,4))
