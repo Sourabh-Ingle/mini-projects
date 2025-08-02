be935849c693e7f5f18c83a91db59cf0a09a471c
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-08-02 23:55:46 +0530

    add two numbers from two linked lists node by node

--- loop-undestanding/linked-list/mergeList.js
+++ loop-undestanding/linked-list/mergeList.js
@@ -16,6 +16,44 @@
 // Input: list1 = [], list2 = [0]
 // Output: [0]
 
+
+// with dummy node
+
+// function ListNode(val,next) {
+//     this.val = val ? val : null;
+//     this.next=next?next:null
+// }
+
+var mergeTwoLists = function (l1, l2) {
+    // 
+    if (!l1) return l2;
+    if (!l2) return l1;
+    
+    let start = new ListNode();
+    let curr = start;
+    
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            curr.next = l1;
+            l1 = l1.next;
+        } else {
+            curr.next = l2;
+            l2 = l2.next;
+        }
+        curr = curr.next;
+    }
+
+    if (!l1) {
+        curr.next = l2;
+    }
+    if (!l2) {
+        curr.next = l1
+    }
+
+    return start;
+
+};
+
 // wirhout taking dummy node any node
 function sortedList(l1, l2) {
     if (!l2) return l1;
