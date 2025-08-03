ab791171f5bd92fa287c573d836d0d7ff72c2753
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-08-03 23:30:44 +0530

    add comments for better  undestanding

--- loop-undestanding/linked-list/duplicatEle.js
+++ loop-undestanding/linked-list/duplicatEle.js
@@ -5,12 +5,13 @@ function noDuplicate(head) {
     let curr = head;
     while (curr && curr.next) {
         if (curr.val === curr.next.val) {
+            // here we are directy goes to next to next node
             curr.next = curr.next.next;
         }
         else {
+            // goes to next node
             curr = curr.next;
         }
     }
     return head;
-        
 }
