58815ea56a363f7d43abeaba3c8e912e4b475e6f
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-08-04 22:36:19 +0530

    SWAP PAIR IN LINK LIST

---
+++ loop-undestanding/linked-list/swapPairNodes.js
@@ -0,0 +1,58 @@
+// 24. Swap Nodes in Pairs
+
+// Given a linked list, swap every two adjacent nodes and return its head.
+// You must solve the problem without modifying the values
+// in the list's nodes (i.e., only nodes themselves may be changed.)
+
+// Example 1:
+// Input: head = [1, 2, 3, 4]
+// Output: [2, 1, 4, 3]
+
+// Example 2:
+// Input: head = []
+// Output: []
+
+// Example 3:
+// Input: head = [1]
+// Output: [1]
+
+// Example 4:
+// Input: head = [1, 2, 3]
+// Output: [2, 1, 3]
+
+// normal iterative way
+let swap = (head) => {
+    if (!head || !head.next) return head;
+
+    let dummy = new ListNode();
+    dummy.next = head;
+
+    let prev = dummy;
+    let curr = head;
+    let node = head.next;
+
+    while (curr && node) {
+        prev.next = node;
+        curr.next = node.next;
+        node.next = curr;
+
+        prev = curr;
+        curr = prev.next;
+        node = curr && curr.next;
+    }
+
+    return dummy.next;
+    
+}
+
+// recursive ways
+
+let swapPair = (head) => {
+    let l = head;
+    let r = head.next;
+
+    l.next = swapPair(r.next);
+    r.next = l;
+    
+    return r;
+}
