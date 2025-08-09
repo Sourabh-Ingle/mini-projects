4ea3d0754593dc6d3a8fcf95a42336e530759ee9
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-08-10 00:03:38 +0530

    2264. Largest 3-Same-Digit Number in String

--- loop-undestanding/strings/threeLargest.js
+++ loop-undestanding/strings/threeLargest.js
@@ -1,17 +1,55 @@
+// 2264. Largest 3-Same-Digit Number in String
+// You are given a string num representing a large integer.
+// An integer is good if it meets the following conditions:
+// It is a substring of num with length 3.
+// It consists of only one unique digit.
+// Return the maximum good integer as a string or an empty string "" if
+//  no such integer exists.
+
+// Note:
+// A substring is a contiguous sequence of characters within a string.
+// There may be leading zeroes in num or a good integer.
+
+// Example 1:
+// Input: num = "6777133339"
+// Output: "777"
+// Explanation: There are two distinct good integers: "777" and "333".
+// "777" is the largest, so we return "777".
+    
+// Example 2:
+// Input: num = "2300019"
+// Output: "000"
+// Explanation: "000" is the only good integer.
+
+// Example 3:
+// Input: num = "42352338"
+// Output: ""
+// Explanation: No substring of length 3 consists of only one unique digit.Therefore, there are no good integers.
+
+// Constraints:
+// 3 <= num.length <= 1000
+// num only consists of digits.
 
 
 
+function largestGoodInteger(num) {
+    let i = 0;
+    let max = "";
 
-var longestCommonPrefix = function (strs) {
-    let x = 0;
-    while (x < strs[0].length) {
-        let ch = strs[0][x];
-        for (let i = 1; i < strs.length; i++) {
-            if (ch !== strs[i][x] || x === strs[i].length) {
-                return strs[0].substring(0, x);
+    while (i <= num.length - 3) {
+        const a = num[i];
+        const b = num[i + 1];
+        const c = num[i + 2];
+
+        if (a === b && b === c) {
+            const candidate = a + b + c;
+            if (max === "" || candidate > max) {
+                max = candidate;
             }
         }
-        x++;
+
+        i++;
     }
-    return strs[0]
-};
+
+    return max;
+}
