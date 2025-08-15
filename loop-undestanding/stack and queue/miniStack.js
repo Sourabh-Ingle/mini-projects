fc4476f8afe8039114b3d737a2ee028a21b15c64
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-08-16 01:17:11 +0530

    Min Stack

---
+++ loop-undestanding/stack and queue/miniStack.js
@@ -0,0 +1,62 @@
+// 155. Min Stack
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+// Implement the MinStack class:
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+// Example 1:
+// Input
+// ["MinStack", "push", "push", "push", "getMin", "pop", "top", "getMin"]
+// [[], [-2], [0], [-3], [], [], [], []]
+
+// Output
+// [null, null, null, null, -3, null, 0, -2]
+
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
+
+// Constraints:
+// -231 <= val <= 231 - 1
+// Methods pop, top and getMin operations will always be called on non - empty stacks.
+// At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+var MinStack = function () {
+    this.stack = [];
+};
+// push
+MinStack.prototype.push = function (val) {
+    if (this.stack.length === 0) {
+        this.stack.push([val, val]);
+    } else {
+        let minVal = this.stack[this.stack.length - 1][1];
+        // console.log(top)
+        if (val > minVal) {
+            this.stack.push([val, minVal]);
+        } else {
+            this.stack.push([val, val]);
+        }
+    }
+};
+// pop
+MinStack.prototype.pop = function () {
+    this.stack.pop();
+};
+// top
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1][0];
+};
+// getMin
+MinStack.prototype.getMin = function () {
+    return this.stack[this.stack.length - 1][1];
+};
