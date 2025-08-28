5b1101421302ceb6b4b5cce06e761bffc48eb853
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-08-28 22:04:14 +0530

    392. Is Subsequence

---
+++ loop-undestanding/slide-pattern/isSubequence.js
@@ -0,0 +1,41 @@
+// 392. Is Subsequence
+
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+// A subsequence of a string is a new string that is formed from the original string by deleting some(can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+// Example 1:
+// Input: s = "abc", t = "ahbgdc"
+// Output: true
+
+// Example 2:
+// Input: s = "axc", t = "ahbgdc"
+// Output: false
+
+// Constraints:
+// 0 <= s.length <= 100
+// 0 <= t.length <= 104
+// s and t consist only of lowercase English letters.
+
+// by using for loop
+let isSubsequence = function (s, t) {
+    let i = 0;
+    for (let j = 0; j < t.length; j++){
+        if (s[i] === t[j]) {
+            i++;
+        }
+    }
+    return i === s.length;
+}
+
+// by using while loop
+let isSubseq = function (s, t) {
+    let i = j = 0;
+    while (j<t.length) {
+        if (s[i] === t[j]) {
+            i++
+        }
+        j++;
+    }
+
+    return i===s.length
+}
