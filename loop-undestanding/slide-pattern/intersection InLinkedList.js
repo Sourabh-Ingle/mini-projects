1d86d0d63122e9da6436c3afeeda0f847919771b
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-09-29 23:30:42 +0530

    Intersection of Two Linked Lists

--- loop-undestanding/slide-pattern/intersection InLinkedList.js
+++ loop-undestanding/slide-pattern/intersection InLinkedList.js
@@ -55,3 +55,15 @@ var getIntersectionNode = function (headA, headB) {
 
 
 }
+
+// OTHER SIMPLE SOLUTION
+var getIntersectionNod = function (headA, headB) {
+    let pA = headA;
+    let pB = headB;
+
+    while (pA != pB) {
+        pA = pA == null ? headB : pA.next;
+        pB = pB == null ? headA : pB.next;
+    }
+    return pA;
+};
