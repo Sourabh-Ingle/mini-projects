f8c3b6a25814fd3c4caeb82a5ba4a8cd715cc1fe
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-10-10 11:08:36 +0530

    ITERATIVE WAY TO HANDLE POSTORDER TRAVESE TREE WITH ONE STACK

--- loop-undestanding/trees/postorderTraversal .js
+++ loop-undestanding/trees/postorderTraversal .js
@@ -15,3 +15,45 @@ var postorderTraversal=function(root){
 
     return ans;
 }
+
+// ITERATIVE WAY TO HANDLE THIS EITH TWO STACK
+var preorderTraversal = function (root) {
+    if (!root) return [];
+    let s1 = [root];
+    let s2 = [];
+    while (s1.length) {
+        let curr = s1.pop();
+        s2.push(curr);
+        curr.left && s1.push(curr.left);
+        curr.right && s1.push(curr.right);
+    }
+
+    while (s2.length) {
+        ans.push(s2.pop().val)
+    }
+    return ans
+}
+
+// // ITERATIVE WAY TO HANDLE POSTORDER TRAVESE TREE WITH ONE STACK
+var preOrderTraversal = function (root) {
+    let stack = [];
+    let ans = [];
+    let curr = root;
+    let peakNode;
+    let visitedValue;
+
+    while (stack.length || curr) {
+        while (curr) {
+            stack.push(curr)
+            curr=curr.left
+        }
+        peakNode = stack[stack.length - 1];
+        if (peakNode.right && peakNode.right !== visitedValue) {
+            curr = peakNode.right;
+        } else {
+            ans.push(peakNode.val);
+            visitedValue = stack.pop()
+        }
+    }
+    return ans;
+}
--- loop-undestanding/trees/preOrderTraversal.js
+++ loop-undestanding/trees/preOrderTraversal.js
@@ -20,20 +20,3 @@ var preorderTraversal = function (root) {
     return ans
 }
 
-// ITERATIVE WAY TO HANDLE THIS 
-var preorderTraversal = function (root) { 
-    if (!root) return [];
-    let s1 = [root];
-    let s2 = [];
-    while (s1.length) {
-        let curr = s1.pop();
-        s2.push(curr);
-        curr.left && s1.push(curr.left);
-        curr.right && s1.push(curr.right);
-    }
-
-    while (s2.length) {
-        ans.push(s2.pop().val)
-    }
-    return ans
-}
