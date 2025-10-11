6edae1b41fce853ddbaae1203540f51d7a8ada15
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-10-11 18:43:20 +0530

    102. Binary Tree Level Order Traversal- ITERATIVE WAY

---
+++ loop-undestanding/trees/levelOrderTraversal.js
@@ -0,0 +1,45 @@
+// 102. Binary Tree Level Order Traversal
+
+// Given the root of a binary tree, return the level order traversal of its nodes' values.
+//     (i.e., from left to right, level by level).
+
+// Example 1:
+// Input: root = [3, 9, 20, null, null, 15, 7]
+// Output: [[3], [9, 20], [15, 7]]
+
+// Example 2:
+// Input: root = [1]
+// Output: [[1]]
+
+// Example 3:
+// Input: root = []
+// Output: []
+
+// Constraints:
+// The number of nodes in the tree is in the range[0, 2000].
+// - 1000 <= Node.val <= 1000
+
+
+// ITERATIVE WAY 
+
+var levelOrder = function (root) {
+    if (!root) return [];
+    let q = [root];
+    let ans = [];
+
+    while (q.lwngth) {
+        let levelArr = [];
+        let qLength = q.length;
+        for (let i = 0; i < qLength; i++){
+            let curr = q.shift();
+            curr.left && q.push(curr.left);
+            curr.right && q.push(curr.right);
+            levelArr.push(curr.val);  
+        }
+        ans.push(levelArr)
+    }
+    return ans;
+}
+
+
+
