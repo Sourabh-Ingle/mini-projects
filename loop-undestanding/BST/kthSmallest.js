78b714d4711fb33e0578ebc8b814b7f4af15fb36
Sourabh Subhash Ingle <Sourabh.ingle15@gmail.com>
2025-10-27 00:42:11 +0530

    230. Kth Smallest Element in a BST-ITERATIVE WAY AS WELL FOR IN ORDER TRAVERSAL

--- loop-undestanding/BST/kthSmallest.js
+++ loop-undestanding/BST/kthSmallest.js
@@ -35,3 +35,19 @@ var kthSmallest = function (root, k) {
     return ans;
 }
 
+// SOLVING THIS IN ITERATIVE WAY OF IN-ORDER TRAVERSAL WAY
+
+var kthSmallest = function (root, k) {
+    let stack = [];
+    let curr = root;
+    while (curr || stack.length) {
+        while (curr) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        k--;
+        if (k == 0) return curr.val;
+        curr = curr.right;
+    }
+}
